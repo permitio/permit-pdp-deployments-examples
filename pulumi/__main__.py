94cc8e7dee72973ba4f3d92856f19145e60d1c64
elimoshkovich <tbhjfo14@gmail.com>
2024-02-29 17:25:49 +0200

    pulumi example python

---
+++ pulumi/__main__.py
@@ -0,0 +1,26 @@
+import pulumi
+from pulumi_kubernetes.core.v1 import Namespace
+from pulumi_kubernetes.helm.v3 import Release, ReleaseArgs, RepositoryOptsArgs
+
+# Create namespace
+pdp_namespace = Namespace("pdp-namespace", metadata={"name": "pdp"})
+
+# Define Pulumi stack
+config = pulumi.Config()
+
+pdp_release = Release(
+    "pdp",
+    ReleaseArgs(
+        chart="pdp",
+        version="0.0.2",
+        namespace=pdp_namespace.metadata["name"],
+        repository_opts=RepositoryOptsArgs(
+            repo="https://permitio.github.io/sidecar"
+        ),
+        values={
+            "pdp": {
+                "ApiKey": config.require_secret("apiKey")
+            }
+        }
+    )
+)
